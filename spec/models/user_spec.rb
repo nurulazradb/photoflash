0a0327d59f04fe305c3377064cee18ed06661831
nurulazradm <nurulazrad@gmail.com>
2018-05-20 10:54:27 +0800

    Creating and running User validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create!(name: 'Test User', email: 'test.user@email.com', password: 'testuser')
+  end
+
+  describe "creation" do
+    it "should have one item created after being created" do
+      expect(User.all.count).to eq(1)
+    end
+  end
+
+  describe "validations" do
+    it "should not let a user be created without an email address" do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "should not let a user be created without a password" do
+      @user.password = nil
+      expect(@user).to_not be_valid
+    end
+  end
 end
